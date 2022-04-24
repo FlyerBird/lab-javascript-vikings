ec3e6d2ff27aeb39641529ecaf0c071d09a85af5
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-04-24 19:01:45 +0200

    test until itineration3

--- src/viking.js
+++ src/viking.js
@@ -1,15 +1,59 @@
 // Soldier
-class Soldier {}
+
+class Soldier {
+  constructor(health, strength) {
+    this.health = health;
+    this.strength = strength;
+  }
+
+  attack() {
+     return this.strength;
+  }
+
+  receiveDamage(damage) {
+      this.health - damage
+  }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+  constructor(name, health, strength) {
+    this.name = name;
+    super(health, strength);
+    }
+
+    receiveDamage(damage){
+      this.health - damage
+      if (Viking === "ALive"){
+        return "A Saxon has received DAMAGE points of damage"
+      }
+    }
+
+    battleCry(){
+       return "Odin Owns You All!"
+    }
+ 
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+
+  receiveDamage(damage){
+    this.health - damage
+    if (Saxon === "ALive"){
+      return "NAME has received DAMAGE points of damage"
+    }
+  }
+}
 
 // War
 class War {}
 
+
+
+
+const Charlie = new Soldier ("100","100");
+console.log (Charlie);
 
 
 // The following is required to make unit tests work.
